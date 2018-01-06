a76148c988154270c4cc6ca5a22bad5bb0e584ff
Mazen Alsarem <m.alsarem@optimiz.me>
2018-01-06 22:21:02 +0100

    full restructure

--- datasets/wikipedia/training-latest-abstract.sh
+++ datasets/wikipedia/training-latest-abstract.sh
@@ -11,10 +11,11 @@ normalize_text() {
 
 perl wikifil.pl $xmlFile >> "${prefix}-text.txt"
 
-normalize_text "${prefix}-text.txt" >> "${prefix}-data.txt"
+#normalize_text "${prefix}-text.txt" >> "${prefix}-data.txt"
 
-../word2phrase -train "${prefix}-data.txt" -output "${prefix}-data-phrase.txt" -threshold 200 -debug 2
-../word2phrase -train d"${prefix}-data-phrase.txt" -output "${prefix}-data-phrase2.txt" -threshold 100 -debug 2
+#../word2phrase -train "${prefix}-data.txt" -output "${prefix}-data-phrase.txt" -threshold 200 -debug 2
+../word2phrase -train "${prefix}-text.txt" -output "${prefix}-data-phrase.txt" -threshold 200 -debug 2
+../word2phrase -train "${prefix}-data-phrase.txt" -output "${prefix}-data-phrase2.txt" -threshold 100 -debug 2
 ../word2vec -train "${prefix}-data-phrase2.txt" -output "${prefix}-vectors.bin" -cbow 1 -size 500 -window 10 -negative 10 -hs 0 -sample 1e-5 -threads 40 -binary 1 -iter 3 -min-count 10
 ../compute-accuracy "${prefix}-vectors.bin" 400000 < ../questions-words.txt     # should get to almost 78% accuracy on 99.7% of questions
 ../compute-accuracy "${prefix}-vectors.bin" 1000000 < ../questions-phrases.txt  # about 78% accuracy with 77% coverage
