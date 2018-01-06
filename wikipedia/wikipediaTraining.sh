f2d3a4db661b6893e46040fbfe98cda441f98da9
Mazen Alsarem <m.alsarem@optimiz.me>
2018-01-06 01:33:45 +0100

    enhance wikipedia generation code

--- wikipedia/wikipediaTraining.sh
+++ wikipedia/wikipediaTraining.sh
@@ -1,3 +1,6 @@
+xmlFile="enwiki-20180101-stub-articles.xml"
+prefix="enwiki-articles"
+
 # This function will convert text to lowercase and remove special characters
 normalize_text() {
   awk '{print tolower($0);}' | sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" -e "s/'/ ' /g" -e "s/“/\"/g" -e "s/”/\"/g" \
@@ -6,57 +9,12 @@ normalize_text() {
   -e 's/«/ /g' | tr 0-9 " "
 }
 
-#for zipfile => gzip -d data/x.gzip |
-#for bzip2 files => bzip2 -c -d data/enwik9.zip |
-#for text
-awk -v f='enwiki-20180101-stub-articles.xml' '{print tolower(f);}' | perl -e '
-# Program to filter Wikipedia XML dumps to "clean" text consisting only of lowercase
-# letters (a-z, converted from A-Z), and spaces (never consecutive)...
-# All other characters are converted to spaces.  Only text which normally appears.
-# in the web browser is displayed.  Tables are removed.  Image captions are.
-# preserved.  Links are converted to normal text.  Digits are spelled out.
-# *** Modified to not spell digits or throw away non-ASCII characters ***
+perl wikifil.pl $xmlFile >> "${prefix}-text.txt"
 
-# Written by Matt Mahoney, June 10, 2006.  This program is released to the public domain.
+normalize_text "${prefix}-text.txt" >> "${prefix}-data.txt"
 
-$/=">";                     # input record separator
-while (<>) {
-  if (/<text /) {$text=1;}  # remove all but between <text> ... </text>
-  if (/#redirect/i) {$text=0;}  # remove #REDIRECT
-  if ($text) {
-
-    # Remove any text not normally visible
-    if (/<\/text>/) {$text=0;}
-    s/<.*>//;               # remove xml tags
-    s/&amp;/&/g;            # decode URL encoded chars
-    s/&lt;/</g;
-    s/&gt;/>/g;
-    s/<ref[^<]*<\/ref>//g;  # remove references <ref...> ... </ref>
-    s/<[^>]*>//g;           # remove xhtml tags
-    s/\[http:[^] ]*/[/g;    # remove normal url, preserve visible text
-    s/\|thumb//ig;          # remove images links, preserve caption
-    s/\|left//ig;
-    s/\|right//ig;
-    s/\|\d+px//ig;
-    s/\[\[image:[^\[\]]*\|//ig;
-    s/\[\[category:([^|\]]*)[^]]*\]\]/[[$1]]/ig;  # show categories without markup
-    s/\[\[[a-z\-]*:[^\]]*\]\]//g;  # remove links to other languages
-    s/\[\[[^\|\]]*\|/[[/g;  # remove wiki url, preserve visible text
-    s/{{[^}]*}}//g;         # remove {{icons}} and {tables}
-    s/{[^}]*}//g;
-    s/\[//g;                # remove [ and ]
-    s/\]//g;
-    s/&[^;]*;/ /g;          # remove URL encoded chars
-
-    $_=" $_ ";
-    chop;
-    print $_;
-  }
-}
-' | normalize_text | awk '{if (NF>1) print;}' >> data.txt
-
-../word2phrase -train data.txt -output data-phrase.txt -threshold 200 -debug 2
-../word2phrase -train data-phrase.txt -output data-phrase2.txt -threshold 100 -debug 2
-../word2vec -train data-phrase2.txt -output vectors.bin -cbow 1 -size 500 -window 10 -negative 10 -hs 0 -sample 1e-5 -threads 40 -binary 1 -iter 3 -min-count 10
-../compute-accuracy vectors.bin 400000 < questions-words.txt     # should get to almost 78% accuracy on 99.7% of questions
-../compute-accuracy vectors.bin 1000000 < questions-phrases.txt  # about 78% accuracy with 77% coverage
+../word2phrase -train "${prefix}-data.txt" -output "${prefix}-data-phrase.txt" -threshold 200 -debug 2
+../word2phrase -train d"${prefix}-data-phrase.txt" -output "${prefix}-data-phrase2.txt" -threshold 100 -debug 2
+../word2vec -train "${prefix}-data-phrase2.txt" -output "${prefix}-vectors.bin" -cbow 1 -size 500 -window 10 -negative 10 -hs 0 -sample 1e-5 -threads 40 -binary 1 -iter 3 -min-count 10
+../compute-accuracy "${prefix}-vectors.bin" 400000 < ../questions-words.txt     # should get to almost 78% accuracy on 99.7% of questions
+../compute-accuracy "${prefix}-vectors.bin" 1000000 < ../questions-phrases.txt  # about 78% accuracy with 77% coverage
